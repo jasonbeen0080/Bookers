40e5aa7c3aca2d48f40588ff098a9db78a91d1ec
jasonbeen0080 <iamnotzurabutkatsura@gmail.com>
2021-09-10 14:24:11 +0000

    終盤

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,28 +11,40 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+     redirect_to book_path(@book.id)
+    else
+     @books = Book.all
+     render :index
+    end
   end
-
-
 
   def edit
     @book = Book.find(params[:id])
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+     @book = Book.find(params[:id])
+       if @book.update(book_params)
+        redirect_to book_path(@book.id)
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "successfully"
+    redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
 
   end
 
